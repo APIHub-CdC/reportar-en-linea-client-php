0e0108257ce390f0f66dfe87e6fe38d5d71d9b07
BryantCancino <bryantcancino@gmail.com>
2019-10-16 14:55:15 -0500

    gitflow-feature-stash: can

---
+++ test/Model/ClaveTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ReportarEnLinea\Client;
+
+class ClaveTest extends \PHPUnit_Framework_TestCase
+{
+    
+    public static function setUpBeforeClass()
+    {
+    }
+    
+    public function setUp()
+    {
+    }
+    
+    public function tearDown()
+    {
+    }
+    
+    public static function tearDownAfterClass()
+    {
+    }
+    
+    public function testClave()
+    {
+    }
+    
+    public function testPropertyClave()
+    {
+    }
+    
+    public function testPropertyDescripcion()
+    {
+    }
+}
---
+++ test/Model/EmpleoTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace ReportarEnLinea\Client;
+
+class EmpleoTest extends \PHPUnit_Framework_TestCase
+{
+    
+    public static function setUpBeforeClass()
+    {
+    }
+    
+    public function setUp()
+    {
+    }
+    
+    public function tearDown()
+    {
+    }
+    
+    public static function tearDownAfterClass()
+    {
+    }
+    
+    public function testEmpleo()
+    {
+    }
+    
+    public function testPropertyNombreEmpresa()
+    {
+    }
+    
+    public function testPropertyDireccion()
+    {
+    }
+    
+    public function testPropertyColoniaPoblacion()
+    {
+    }
+    
+    public function testPropertyDelegacionMunicipio()
+    {
+    }
+    
+    public function testPropertyCiudad()
+    {
+    }
+    
+    public function testPropertyEstado()
+    {
+    }
+    
+    public function testPropertyCp()
+    {
+    }
+    
+    public function testPropertyNumeroTelefono()
+    {
+    }
+    
+    public function testPropertyExtension()
+    {
+    }
+    
+    public function testPropertyFax()
+    {
+    }
+    
+    public function testPropertyPuesto()
+    {
+    }
+    
+    public function testPropertyFechaContratacion()
+    {
+    }
+    
+    public function testPropertyClaveMoneda()
+    {
+    }
+    
+    public function testPropertySalarioMensual()
+    {
+    }
+    
+    public function testPropertyFechaUltimoDiaEmpleo()
+    {
+    }
+    
+    public function testPropertyFechaVerificacionEmpleo()
+    {
+    }
+    
+    public function testPropertyOrigenRazonSocialDomicilio()
+    {
+    }
+}
